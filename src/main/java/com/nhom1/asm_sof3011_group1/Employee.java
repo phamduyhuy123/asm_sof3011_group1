fc02981e9f961860a20b9151f8acaf5dee371159
duyhuy <huypdps24924@fpt.edu.vn>
2023-03-11 00:24:07 +0700

    setup lombok danh rieng cho eclipse bằng cách chạy file lombok.jar

--- src/main/java/com/nhom1/asm_sof3011_group1/Employee.java
+++ src/main/java/com/nhom1/asm_sof3011_group1/Employee.java
@@ -3,13 +3,14 @@ package com.nhom1.asm_sof3011_group1;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+
 public class Employee {
     private int id;
     private String name;
--- src/main/java/com/nhom1/asm_sof3011_group1/HelloServlet.java
+++ src/main/java/com/nhom1/asm_sof3011_group1/HelloServlet.java
@@ -3,9 +3,7 @@ package com.nhom1.asm_sof3011_group1;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.google.gson.Gson;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,7 +19,10 @@ public class HelloServlet extends HttpServlet {
         response.setContentType("application/json");
 
         PrintWriter out = response.getWriter();
+        Employee emp=new Employee();
+        
         Gson gson=new Gson();
+        
         List<Employee> employees=new ArrayList<>();
         employees.add(Employee.builder().id(1).name("wert").build());
         employees.add(Employee.builder().id(2).name("wefewf").build());
